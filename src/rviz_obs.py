f6c686a2db55f52c93bb724eba4bb0dd312b7da6
new2me321 <kwasiboatjr@live.co.uk>
2023-04-04 15:27:23 +0300

    added files

--- src/rviz_obs.py
+++ src/rviz_obs.py
@@ -17,7 +17,6 @@ cwd = os.getcwd()
 
 def cylinder_pose_callback(msg):
     # Find the index of the cylinders in the ModelStates message
-
     cylinder_indices = [msg.name.index(
         'unit_cylinder_{}'.format(i)) for i in range(len(msg.name)-3)]
 
@@ -42,7 +41,6 @@ def visualize_obstacles():
         marker_msg.action = Marker.ADD
 
         # Do transformations
-
         r, p, y = euler_from_quaternion([marker_msg.pose.orientation.x, marker_msg.pose.orientation.y,
                                         marker_msg.pose.orientation.z, marker_msg.pose.orientation.w])
         Q = quaternion_from_euler(r, p, y-1.57)
@@ -53,7 +51,7 @@ def visualize_obstacles():
         transform.transform.rotation.z = Q[2]
         transform.transform.rotation.w = Q[3]
 
-        # create Vector3Stamped object for original pose
+        # We will use Vector3Stamped object for original pose
         v_orig = Vector3Stamped()
         v_orig.header.stamp = rospy.Time.now()
         v_orig.header.frame_id = 'map'
@@ -61,7 +59,7 @@ def visualize_obstacles():
         v_orig.vector.y = cylinder_poses[i].position.y
         v_orig.vector.z = 0.25
 
-        # transform Vector3Stamped object using Transform object
+        # transform Vector3Stamped
         v_transformed = do_transform_vector3(v_orig, transform)
 
         marker_msg.pose.position = v_transformed.vector
@@ -86,20 +84,20 @@ def visualize_obstacles():
         marker_pub.publish(marker_msg)
 
 
+get_cylinder_poses = False
 if __name__ == '__main__':
     # Initialize the ROS node
     rospy.init_node('cylinder_marker_publisher')
     rospy.loginfo_once('visualizing obstacles')
 
-    # Create a ROS publisher for the cylinder markers
-    marker_pub = rospy.Publisher('/cylinder_markers', Marker, queue_size=10)
-    loop_rate = 10
-    # Subscribe to the Gazebo model states topic
-    # rospy.Subscriber('/gazebo/model_states', ModelStates, cylinder_pose_callback)
-    while not rospy.is_shutdown():
-        visualize_obstacles()
-        # rospy.sleep(1)
-        # rospy.spin()
-
-    # Spin the node
-    # rospy.spin()
+    if get_cylinder_poses == True:
+        # Subscribe to the Gazebo model states topic
+        rospy.Subscriber('/gazebo/model_states', ModelStates, cylinder_pose_callback)
+        rospy.spin()
+    else:
+        marker_pub = rospy.Publisher('/cylinder_markers', Marker, queue_size=10)
+        loop_rate = 10
+        while not rospy.is_shutdown():
+            visualize_obstacles()
+            # rospy.sleep(1)
+            # rospy.spin()
--- src/rviz_vehicle.py
+++ src/rviz_vehicle.py
@@ -3,7 +3,6 @@
 import rospy
 import os
 import numpy as np
-import rospkg
 from tf.transformations import quaternion_from_euler
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
@@ -14,30 +13,26 @@ cwd = os.getcwd()
 jackal_odometry = np.load(os.path.join(cwd, "data/jackal_odometry.npy"))
 obs_points = np.load(os.path.join(cwd, "data/original_pointclouds.npy"))
 
-
 rospy.init_node('robot_marker_publisher')
-
-rospack = rospkg.RosPack()
-package_path = rospack.get_path('jackal_description')
 
 robot_marker_pub = rospy.Publisher('robot_visualizer', Marker, queue_size=1)
 pcl_pub = rospy.Publisher('my_point_cloud_topic', PointCloud, queue_size=10)
 
 loop_rate = 10  # in hertz
-x_offset = 3 #-2
+x_offset = 3  # -2
 y_offset = 2
 
+# jackal pose.position
+robot_x = jackal_odometry[:, 0]
+robot_y = jackal_odometry[:, 1]
+robot_yaw = jackal_odometry[:, 2]
+
 # Prepare pointcloud data
-# flatten the point array and broadcast the z values
 flat_points = obs_points.reshape(-1, 2)
 z_values = 0.3 * np.ones_like(flat_points[:, 0])
 z_values = z_values.reshape(obs_points.shape[0], -1)
 z_values = z_values[:, :, np.newaxis]
 obs_points = np.concatenate((obs_points, z_values), axis=2)
-
-robot_x = jackal_odometry[:, 0]
-robot_y = jackal_odometry[:, 1]
-robot_yaw = jackal_odometry[:, 2]
 
 # pre generate pointscloud points message
 points_list = []
@@ -59,7 +54,6 @@ while not rospy.is_shutdown():
         point_cloud_msg = PointCloud()
         point_cloud_msg.header.stamp = rospy.Time.now()
         point_cloud_msg.header.frame_id = 'world'
-
         point_cloud_msg.points = points_list[i]
 
         # Robot
@@ -84,6 +78,7 @@ while not rospy.is_shutdown():
         marker_msg.scale.x = 0.508
         marker_msg.scale.y = 0.430
         marker_msg.scale.z = 0.250
+
         # color - yellow
         marker_msg.color.r = 1.0
         marker_msg.color.g = 1.0
